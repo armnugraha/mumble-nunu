e02a0eb8cbd0f610ff78407447e3b6e1c0fd4210
arm_nugraha <armnugraha@envisions.app>
2024-07-16 19:42:01 +0700

    init

---
+++ cd9ef27293fbc92f257e.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./loc/de.json":
+/*!*********************!*\
+  !*** ./loc/de.json ***!
+  \*********************/
+/*! exports provided: connectdialog, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"connectdialog\":{\"title\":\"Verbindung herstellen\",\"address\":\"Adresse\",\"port\":\"Port\",\"username\":\"Nutzername\",\"password\":\"Passwort\",\"tokens\":\"Tokens\",\"remove\":\"Entfernen\",\"add\":\"Hinzufügen\",\"cancel\":\"Abbrechen\",\"connect\":\"Verbinden\"}}");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=cd9ef27293fbc92f257e.js.map
---
+++ ed39e260bf6831c24ea9.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "./loc/eo.json":
+/*!*********************!*\
+  !*** ./loc/eo.json ***!
+  \*********************/
+/*! exports provided: connectdialog, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"connectdialog\":{\"title\":\"Konektado\",\"address\":\"Adreso\",\"port\":\"Pordo\",\"username\":\"Uzantnomo\",\"password\":\"Pasvorto\",\"tokens\":\"Ĵetonoj\",\"remove\":\"Forigi\",\"add\":\"Aldoni\",\"cancel\":\"Nuligi\",\"connect\":\"Konekti\"}}");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=ed39e260bf6831c24ea9.js.map
